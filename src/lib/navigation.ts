2ac8a6ecfb44f524c26e033d24d183f0edec84a2
Ellen Landrum <elandrum@usc.edu>
2025-07-13 20:15:25 -0700

    fuck jeremy for not communicating about already doing footer/nav

---
+++ src/lib/navigation.ts
@@ -0,0 +1,18 @@
+export const MenuItems = [
+    {
+      title: 'Voices',
+      url: '/voices',
+    },
+    {
+      title: 'Resources',
+      url: '/resources',
+    },
+    {
+      title: 'About',
+      url: '/about',
+    },
+    {
+      title: 'Landscapes',
+      url: '/landscapes',
+    },
+];
